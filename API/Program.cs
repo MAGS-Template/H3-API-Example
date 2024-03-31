9e283c504935098d92eb5271bc5be49f27ecc2b9
MAGS <120091142+Mercantec-MAGS@users.noreply.github.com>
2024-03-31 11:40:58 +0200

    AddEnvironmentVariables

--- API/Program.cs
+++ API/Program.cs
@@ -14,13 +14,13 @@ namespace H3_PostgresRESTFulAPI
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Configuration.AddEnvironmentVariables();
 
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? Environment.GetEnvironmentVariable("DefaultConnection");
-
+            string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
             builder.Services.AddDbContext<AppDBContext>(options =>
                 options.UseNpgsql(connectionString));
 
